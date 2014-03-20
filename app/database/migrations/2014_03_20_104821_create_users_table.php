8ae5c15c28d145d7c21248165be1ed47c24e96c3
Dr.Yami <cuongnt.hn@gmail.com>
2014-03-20 14:20:33 +0200

    Database: users, rooms & groups

---
+++ app/database/migrations/2014_03_20_104821_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('users', function($table) {
+			$table->increment('id');
+			$table->string('username')->unique();
+			$table->string('password');
+			$table->integer('group_id')->unsigned();
+			$table->integer('room_id')->unsigned()->nullable();
+			$table->boolean('active')->default(true);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::drop('users');
+	}
+
+}
---
+++ app/database/migrations/2014_03_20_110814_create_rooms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('rooms', function($table) {
+			$table->increment('id');
+			$table->string('room_code');
+			$table->boolean('active');
+			$table->integer('person_count');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
