17839c82cdc203a72528aa451c6be4392a7852e8
Dr.Yami <cuongnt.hn@gmail.com>
2014-04-01 18:07:31 +0300

    Prepared for sprint 2

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -5,7 +5,7 @@ class HomeController extends BaseController {
     protected $layout = "layout.master";
 
     public function index() {
-
+        $this->layout->body = View::make('ascii');
     }
 
 }
