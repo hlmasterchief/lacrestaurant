4f3ed2810add39766a0dd009e8a0987faf01aa3e
Dr.Yami <cuongnt.hn@gmail.com>
2014-04-09 09:11:28 +0300

    menu template

--- public/js/main.js
+++ public/js/main.js
@@ -16,13 +16,22 @@ lacApp.controller("HomeController", function($scope, $http) {
     
 });
 
+// MenuController
+lacApp.controller("MenuController", function($scope, $http) {
+    
+});
+
 // route setting
 lacApp.config(function($routeProvider, $locationProvider) {
     $routeProvider
         // route for the home page
         .when('/', {
-            templateUrl : 'template/ascii.html',
+            templateUrl : 'template/about.html',
             controller  : 'HomeController'
+        })
+        .when('/menu', {
+            templateUrl : 'template/menu.html',
+            controller  : 'MenuController'
         })
         // redirect if route not found
         .otherwise({
