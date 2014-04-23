d8f3a6eb871b87972bca8c2dd493559fc63b3660
Dr.Yami <cuongnt.hn@gmail.com>
2014-04-23 08:47:31 +0300

    seed for testing

--- app/database/seeds/MenuTableSeeder.php
+++ app/database/seeds/MenuTableSeeder.php
@@ -14,6 +14,14 @@ class MenuTableSeeder extends Seeder {
             $menu->dishes()->save($dish);
         }
 
+        $menu = new Menu();
+        $menu->menu_date = "2014-04-23";
+        $menu->save();
+
+        foreach (Dish::all() as $dish) {
+            $menu->dishes()->save($dish);
+        }
+
         $recommendation = new Recommendation();
         $recommendation->menu_id = $menu->id;
         $recommendation->recommendation = "Today, we has abcxyz";
